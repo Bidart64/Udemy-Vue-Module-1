2c3e32dd0b565e93cf8c3ef4dcdbc22eeb1abca7
veytiah <hayden@chainstarters.com>
2022-04-19 11:48:36 +0200

    different folder structure

--- section-9/src/App.vue
+++ section-9/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <the-header></the-header>
-    <!-- <badge-list></badge-list>
+    <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
@@ -9,7 +9,7 @@
     ></user-info>
     <course-goals #default="slotProps">
       <h2>{{ slotProps.item }}</h2>
-    </course-goals> -->
+    </course-goals>
     <button @click="setselectComponent(`active-goals`)">Active Goals</button>
     <button @click="setselectComponent(`manage-goals`)">Manage Goals</button>
     <!-- <active-goals v-if="selectedComponent === `active-goals`"></active-goals>
@@ -21,19 +21,19 @@
 </template>
 
 <script>
-import TheHeader from "./components/TheHeader.vue";
-// import UserInfo from "./components/UserInfo.vue";
-// import BadgeList from "./components/BadgeList.vue";
-// import CourseGoals from "./components/CourseGoals.vue";
+import TheHeader from "./components/layout/layout/layout/TheHeader.vue";
+import UserInfo from "./components/UserInfo.vue";
+import BadgeList from "./components/UI/BadgeList.vue";
+import CourseGoals from "./components/CourseGoals.vue";
 import ActiveGoals from "./components/ActiveGoals.vue";
 import ManageGoals from "./components/ManageGoals.vue";
 
 export default {
   components: {
     TheHeader,
-    // UserInfo,
-    // BadgeList,
-    // CourseGoals,
+    UserInfo,
+    BadgeList,
+    CourseGoals,
     ActiveGoals,
     ManageGoals,
   },
