bc7ddd821edca0669c80af18095649f8653c2140
veytiah <hayden@chainstarters.com>
2022-04-19 10:31:21 +0200

    scoped styles

--- section-9/src/components/BadgeList.vue
+++ section-9/src/components/BadgeList.vue
@@ -28,4 +28,17 @@ ul {
 li {
   margin-right: 1rem;
 }
+section {
+  margin: 2rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+}
+
+section header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 </style>
